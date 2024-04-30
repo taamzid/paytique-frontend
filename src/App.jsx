0ce40a3ef1549312a206f967f07b2e89b0106020
taamzid <tamjidalam00@gmail.com>
2024-05-01 00:25:38 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p className="read-the-docs text-[70px]">
+        PAYTIQUE
+      </p>
+    </>
+  );
+}
+
+export default App;
