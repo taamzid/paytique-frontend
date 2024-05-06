1e248112d32c78169ddfd345d0d406b9fbda517e
taamzid <tamjidalam00@gmail.com>
2024-05-07 00:52:03 +0600

    ui-update

--- src/components/Home/NavBar/NavBar.jsx
+++ src/components/Home/NavBar/NavBar.jsx
@@ -12,15 +12,25 @@ const NavBar = () => {
       </div>
       <div className="flex items-center text-[#ffffff]">
         <div className="flex items-center smd:gap-[30px] smd:pl-[200px] lg:pl-[300px] lg:gap-[64px] smd:text-[14px] lg:text-[16px] font-SFSemiBold">
-          <h1 className="cursor-pointer">Home</h1>
-          <h1 className="cursor-pointer">Company</h1>
-          <h1 className="cursor-pointer">Product</h1>
-          <h1 className="cursor-pointer">Support</h1>
+          <h1 className="cursor-pointer hover:underline underline-offset-[20px] decoration-[3px] decoration-[#196BFE]">
+            Home
+          </h1>
+          <h1 className="cursor-pointer hover:underline underline-offset-[20px] decoration-[3px] decoration-[#196BFE]">
+            Company
+          </h1>
+          <h1 className="cursor-pointer hover:underline underline-offset-[20px] decoration-[3px] decoration-[#196BFE]">
+            Product
+          </h1>
+          <h1 className="cursor-pointer hover:underline underline-offset-[20px] decoration-[3px] decoration-[#196BFE]">
+            Support
+          </h1>
         </div>
       </div>
       <div className="flex items-center text-[#ffffff] smd:pl-[220px] lg:pl-[352px] smd:gap-[30px] lg:gap-[42px] smd:text-[14px] lg:text-[16px] font-SFSemiBold">
         <h1 className="cursor-pointer">Log In</h1>
-        <button className="text-center text-[#003AA1] rounded-full font-bold bg-[#ffffff] w-[154px] h-[56px]">Register Now</button>
+        <button className="text-center text-[#003AA1] rounded-full font-bold bg-[#ffffff] w-[154px] h-[56px]">
+          Register Now
+        </button>
       </div>
     </div>
   );
