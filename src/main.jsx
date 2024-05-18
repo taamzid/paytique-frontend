39a18d0c367e71c1418a5fce7053dcc3c0ecc465
taamzid <tamjidalam00@gmail.com>
2024-05-18 21:35:33 +0600

    half-dash

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,12 @@ import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 //prod
-// const domain = "paytique.us.auth0.com";
-// const clientId = "Lf57nQnjJIz4zN0hzcC2bdW8WRpJxvE5";
+const domain = "paytique.us.auth0.com";
+const clientId = "Lf57nQnjJIz4zN0hzcC2bdW8WRpJxvE5";
 
 //dev
-const domain = "tamzid.us.auth0.com";
-const clientId = "myHhzPMD7RMB8enKjAWleJwYlXsmOlNs";
+// const domain = "tamzid.us.auth0.com";
+// const clientId = "myHhzPMD7RMB8enKjAWleJwYlXsmOlNs";
 
 const redirectUri = `${window.location.origin}/dashboard`;
 
