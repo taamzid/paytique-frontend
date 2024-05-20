0d00377c444e571a63c51dc77142348f943dbd1e
taamzid <tamjidalam00@gmail.com>
2024-05-21 02:53:41 +0600

    mob-dash

--- src/components/Dashboard/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard/Dashboard.jsx
@@ -7,7 +7,7 @@ export const Dashboard = () => {
   return (
     <div className="bg-[#ffffff] smd:grid smd:grid-cols-[1fr_2.5fr_1.5fr] slg:grid-cols-[.9fr_3fr_1fr] lg:grid-cols-[.8fr_3fr_1.1fr] h-[100vh]">
       <LeftDash />
-      <div className="overflow-y-auto pb-[40px] [&::-webkit-scrollbar]:hidden [-ms-overflow-style]:hidden [scrollbar-width]:hidden">
+      <div className="overflow-y-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style]:hidden [scrollbar-width]:hidden">
         <div className="hidden smd:block">
           <Common />
         </div>
