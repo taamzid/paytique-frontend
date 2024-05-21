d0ffdcc2bc698c22cefda15015a8de109024835a
taamzid <tamjidalam00@gmail.com>
2024-05-22 00:41:59 +0600

    ui-update

--- src/components/Home/Find/Find.jsx
+++ src/components/Home/Find/Find.jsx
@@ -134,7 +134,7 @@ const Find = () => {
         className="hidden smd:block smd:absolute right-0"
       />
       <div className="bg-[#ffffff]">
-        <div className="mt-[60px] smd:mt-[340px] slg:mt-[400px] lg:mt-[470px] relative">
+        <div className="pt-[60px] smd:pt-[0px] smd:mt-[340px] slg:mt-[400px] lg:mt-[470px] relative">
           <div>
             <h1 className="text-[42px] smd:text-[60px] lg:text-[80px] text-[#1E1E1E] text-center font-SFMedium">
               How to Get Started
