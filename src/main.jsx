9926bcae2c89e4f9ffb26dff96fcd87e39d0854b
taamzid <tamjidalam00@gmail.com>
2024-05-22 14:02:46 +0600

    ui-update

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,12 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 
 //prod
-// const domain = "paytique.us.auth0.com";
-// const clientId = "Lf57nQnjJIz4zN0hzcC2bdW8WRpJxvE5";
+const domain = "paytique.us.auth0.com";
+const clientId = "Lf57nQnjJIz4zN0hzcC2bdW8WRpJxvE5";
 
 //dev
-const domain = "tamzid.us.auth0.com";
-const clientId = "myHhzPMD7RMB8enKjAWleJwYlXsmOlNs";
+// const domain = "tamzid.us.auth0.com";
+// const clientId = "myHhzPMD7RMB8enKjAWleJwYlXsmOlNs";
 
 const redirectUri = `${window.location.origin}/dashboard`;
 
